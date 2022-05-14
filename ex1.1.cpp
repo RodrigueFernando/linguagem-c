b6b1af6af850f8c178422ff2b43049c87badfa5b
RodrigueFernando <90278130+RodrigueFernando@users.noreply.github.com>
2022-05-14 00:44:21 -0300

    Add files via upload

---
+++ ex1.1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main(){
+	int idade1;
+	int idade2;
+	int idade3;
+	
+	printf("Informe  idade 1:");
+	scanf("%d",&idade1);
+	
+	printf("informe a idade2:");
+	scanf("%d",&idade2);
+	
+	printf("informe a idade3:");
+	scanf("%d",&idade3);
+	
+	printf(" idade1 %d  \n idade2 %d \n idade3 %d",idade1 ,idade2,idade3 );
+	
+}
