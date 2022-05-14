674bec8278ada4e97daea31b57649a870ce21b0c
RodrigueFernando <90278130+RodrigueFernando@users.noreply.github.com>
2022-05-14 00:48:15 -0300

    Add files via upload

---
+++ ex1.5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main(){
+	int  lado;
+	int  A;
+	printf(" informe  um  lado  do  quadrado:");
+	scanf("%d",&lado);
+	
+	A = lado * lado;
+	
+	printf("Area do  quadrado: %d",A);
+}
